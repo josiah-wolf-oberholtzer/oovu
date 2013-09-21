c04227996c9b9f7b46c021420dfa12158a0c6918
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-09-21 15:21:37 -0400

    Wrote tests for OSC address node pruning logic.

--- java/oovu/src/oovu/environment/tests/OscAddressNodeTest.java
+++ java/oovu/src/oovu/environment/tests/OscAddressNodeTest.java
@@ -9,8 +9,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class OscAddressNodeTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		Environment.reset();
+	}
 
-	public void create_osc_address_nodes() {
+	@After
+	public void tearDown() throws Exception {
+		Environment.reset();
+	}
+	
+	@Test
+	public void test_01() {
 		OscAddressNode root = Environment.root_osc_address_node;
 		OscAddressNode module_one = new OscAddressNode("module.one");
 		OscAddressNode module_one_dsp = new OscAddressNode("dsp");
@@ -30,23 +41,8 @@ public class OscAddressNodeTest {
 		module_two_dsp.add_child(module_two_dsp_gain);
 	}
 	
-	@Before
-	public void setUp() throws Exception {
-		Environment.reset();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		Environment.reset();
-	}
-	
 	@Test
-	public void test_create_nodes_01() {
-		this.create_osc_address_nodes();
-	}
-	
-	@Test
-	public void test_create_nodes_02() {
+	public void test_02() {
 		OscAddressNode foo = new OscAddressNode("foo");
 
 		assertArrayEquals(foo.get_parentage(), new OscAddressNode[]{ foo });
@@ -55,6 +51,7 @@ public class OscAddressNodeTest {
 		assertEquals(foo.get_reference_count(), 0);
 		assertEquals(foo.get_root(), foo);
 		assertEquals(foo.is_empty(), true);
+		assertEquals(foo.name, "foo");
 
 		OscAddressNode bar = new OscAddressNode("bar");
 		foo.add_child(bar);
@@ -67,7 +64,60 @@ public class OscAddressNodeTest {
 
 		assertArrayEquals(bar.get_parentage(), new OscAddressNode[]{ bar, foo });
 		assertEquals(bar.get_parent(), foo);
+		assertEquals(bar.get_reference_count(), 0);
 		assertEquals(bar.get_root(), foo);
+		assertEquals(bar.is_empty(), true);
+		assertEquals(bar.name, "bar");
+
+		foo.remove_child(bar);
+		
+		assertArrayEquals(foo.get_parentage(), new OscAddressNode[]{ foo });
+		assertEquals(foo.get_child("bar"), null);
+		assertEquals(foo.get_parent(), null);
+		assertEquals(foo.get_reference_count(), 0);
+		assertEquals(foo.get_root(), foo);
+		assertEquals(foo.is_empty(), true);
+		assertEquals(foo.name, "foo");
+		
+		assertArrayEquals(bar.get_parentage(), new OscAddressNode[]{ bar });
+		assertEquals(bar.get_parent(), null);
+		assertEquals(bar.get_reference_count(), 0);
+		assertEquals(bar.get_root(), bar);
+		assertEquals(bar.is_empty(), true);
+		assertEquals(bar.name, "bar");
+	}
+	
+	@Test
+	public void test_03() {
+		OscAddressNode foo = new OscAddressNode("foo");
+		assertEquals(foo.is_in_environment(), false);
+		Environment.root_osc_address_node.add_child(foo);
+		assertEquals(foo.is_in_environment(), true);
+		Environment.root_osc_address_node.remove_child(foo);
+		assertEquals(foo.is_in_environment(), false);
+	}
+	
+	@Test
+	public void test_04() {
+		OscAddressNode foo = new OscAddressNode("foo");
+		OscAddressNode bar = new OscAddressNode("bar");
+		OscAddressNode baz = new OscAddressNode("baz");
+		OscAddressNode quux = new OscAddressNode("quux");
+		foo.add_child(bar);
+		bar.add_child(baz);
+		bar.add_child(quux);
+		assertArrayEquals(baz.get_parentage(), new OscAddressNode[]{ baz, bar, foo });
+		assertArrayEquals(quux.get_parentage(), new OscAddressNode[]{ quux, bar, foo });
+		foo.prune();
+		assertArrayEquals(baz.get_parentage(), new OscAddressNode[]{ baz, bar, foo });
+		assertArrayEquals(quux.get_parentage(), new OscAddressNode[]{ quux, bar, foo });
+		bar.prune();
+		assertArrayEquals(baz.get_parentage(), new OscAddressNode[]{ baz, bar, foo });
+		assertArrayEquals(quux.get_parentage(), new OscAddressNode[]{ quux, bar, foo });
+		baz.prune();
+		assertArrayEquals(baz.get_parentage(), new OscAddressNode[]{ baz });
+		assertArrayEquals(quux.get_parentage(), new OscAddressNode[]{ quux, bar, foo });
+		quux.prune();
 	}
 
 }
