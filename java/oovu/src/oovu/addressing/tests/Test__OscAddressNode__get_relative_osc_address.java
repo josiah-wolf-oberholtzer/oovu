23d889b8663167ee65387728a5e8ec9cc1645ce0
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-09-27 16:42:34 -0400

    Migrated Response.get_relative_osc_address() logic to OscAddressNode.get_relative_osc_address().

---
+++ java/oovu/src/oovu/addressing/tests/Test__OscAddressNode__get_relative_osc_address.java
@@ -0,0 +1,35 @@
+package oovu.addressing.tests;
+
+import oovu.addressing.OscAddress;
+import oovu.addressing.OscAddressNode;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test__OscAddressNode__get_relative_osc_address {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        OscAddressNode root = new OscAddressNode("");
+        OscAddressNode foo = root.create_address(OscAddress.from_cache("/foo"),
+            false);
+        OscAddressNode bar = root.create_address(
+            OscAddress.from_cache("/foo/bar"), false);
+        Assert.assertEquals("", root.get_relative_osc_address(root));
+        Assert.assertEquals("/foo", foo.get_relative_osc_address(root));
+        Assert.assertEquals("", foo.get_relative_osc_address(foo));
+        Assert.assertEquals("/foo/bar", bar.get_relative_osc_address(root));
+        Assert.assertEquals("/bar", bar.get_relative_osc_address(foo));
+        Assert.assertEquals("", bar.get_relative_osc_address(bar));
+    }
+}
--- java/oovu/src/oovu/messaging/Response.java
+++ java/oovu/src/oovu/messaging/Response.java
@@ -3,8 +3,6 @@ package oovu.messaging;
 import oovu.addressing.OscAddressNode;
 import oovu.servers.Server;
 
-import org.apache.commons.lang3.ArrayUtils;
-
 import com.cycling74.max.Atom;
 
 abstract public class Response {
@@ -21,24 +19,7 @@ abstract public class Response {
 
     public String get_relative_osc_address(
         OscAddressNode relative_osc_address_node) {
-        OscAddressNode[] source_parentage = this.source.get_osc_address_node()
-            .get_parentage();
-        OscAddressNode[] relative_parentage = relative_osc_address_node
-            .get_parentage();
-        ArrayUtils.reverse(source_parentage);
-        ArrayUtils.reverse(relative_parentage);
-        int counter = 0;
-        while ((counter < source_parentage.length)
-            && (counter < relative_parentage.length)
-            && (source_parentage[counter] == relative_parentage[counter])) {
-            counter += 1;
-        }
-        StringBuilder string_builder = new StringBuilder();
-        while (counter < source_parentage.length) {
-            string_builder.append("/");
-            string_builder.append(source_parentage[counter].get_name());
-            counter += 1;
-        }
-        return string_builder.toString();
+        return this.source.get_osc_address_node().get_relative_osc_address(
+            relative_osc_address_node);
     }
 }
