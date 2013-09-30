79d3434530f37e2fd5e523e140885c8b0ea6f92f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-09-30 16:27:40 -0400

    Implemented a multi-segment line class.
    oovu.timewise.Line should behave identically to Max's [line~], albeit
    not in the signal domain.

--- java/oovu/src/oovu/timewise/Line.java
+++ java/oovu/src/oovu/timewise/Line.java
@@ -1,5 +1,7 @@
 package oovu.timewise;
 
+import java.util.ArrayList;
+
 import com.cycling74.max.Atom;
 import com.cycling74.max.Executable;
 import com.cycling74.max.MaxClock;
@@ -7,57 +9,97 @@ import com.cycling74.max.MaxObject;
 
 public class Line extends MaxObject implements Executable {
 
+    private class TimePoint {
+
+        public final double time;
+        public final double value;
+
+        public TimePoint(double time, double value) {
+            this.time = time;
+            this.value = value;
+        }
+    }
+
     private MaxClock clock = new MaxClock(this);
     private int output_granularity = 20;
-    private float remaining_time = 0;
-    private float current_value = 0;
-    private float starting_value = 0;
-    private float desired_value = 0;
-    private float current_slope = 0;
+    private ArrayList<TimePoint> time_points;
 
     public Line(Atom[] arguments) {
         this.declareIO(1, 1);
-        if ((0 < arguments.length) && !arguments[0].isString()) {
-            this.current_value = arguments[0].toInt();
+        this.time_points = new ArrayList<TimePoint>();
+        double value = 0.;
+        if (0 < arguments.length) {
+            value = arguments[0].toDouble();
         }
+        double current_time = System.currentTimeMillis();
+        this.time_points.add(new TimePoint(current_time, value));
     }
 
     @Override
     public void bang() {
-        this.outlet(0, this.current_value);
+        double current_time = System.currentTimeMillis();
+        this.outlet(0, (float) this.find_value_at_time(current_time));
     }
 
     @Override
     public void execute() {
-        float next_time = this.remaining_time - this.output_granularity;
-        if (next_time < 0) {
-            next_time = 0;
-        }
-        float next_value = this.desired_value
-            + (this.current_slope * next_time);
-        this.current_value = (int) next_value;
-        this.remaining_time = next_time;
-        this.outlet(0, this.current_value);
-        if (0 < next_time) {
+        double current_time = System.currentTimeMillis();
+        this.outlet(0, this.find_value_at_time(current_time));
+        if (1 < this.time_points.size()) {
             this.clock.delay(this.output_granularity);
         }
+    }
+
+    public double find_value_at_time(double current_time) {
+        while ((1 < this.time_points.size())
+            && ((this.time_points.get(1).time) < current_time)) {
+            this.time_points.remove(0);
+        }
+        if ((this.time_points.size() == 2)
+            && (this.time_points.get(1).time < current_time)) {
+            this.time_points.remove(0);
+        }
+        if (this.time_points.size() == 1) {
+            return this.time_points.get(0).value;
+        }
+        TimePoint one = this.time_points.get(0);
+        TimePoint two = this.time_points.get(1);
+        double delta_value = two.value - one.value;
+        double delta_time = two.time - one.time;
+        double slope = delta_value / delta_time;
+        return (slope * (current_time - one.time)) + one.value;
+    }
+
+    @Override
+    public void inlet(float value) {
+        this.stop();
+        double current_time = System.currentTimeMillis();
+        this.time_points.clear();
+        this.time_points.add(new TimePoint(current_time, value));
+        this.outlet(0, (double) value);
     }
 
     @Override
     public void inlet(int value) {
         this.stop();
-        this.current_value = value;
-        this.outlet(0, value);
+        double current_time = System.currentTimeMillis();
+        this.time_points.clear();
+        this.time_points.add(new TimePoint(current_time, value));
+        this.outlet(0, (double) value);
     }
 
     @Override
-    public void list(int[] value) {
+    public void list(float[] values) {
         this.stop();
-        this.desired_value = value[0];
-        this.remaining_time = value[1];
-        this.starting_value = this.current_value;
-        this.current_slope = (this.starting_value - this.desired_value)
-            / this.remaining_time;
+        double time = System.currentTimeMillis();
+        double value = this.find_value_at_time(time);
+        this.time_points.clear();
+        this.time_points.add(new TimePoint(time, value));
+        for (int i = 0, j = values.length; i < (j - 1); i += 2) {
+            value = values[i];
+            time = values[i + 1] + time;
+            this.time_points.add(new TimePoint(time, value));
+        }
         this.clock.delay(this.output_granularity);
     }
 
--- java/oovu/src/oovu/timewise/LineSegment.java
+++
@@ -1,4 +0,0 @@
-package oovu.timewise;
-
-public class LineSegment {
-}
