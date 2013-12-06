6322be01cf08a8ed8b6fdd567d0c01654c187b31
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-12-06 12:22:00 -0500

    Added MidiFilter for MidiEvents.

---
+++ java-source/oovu/src/oovu/events/MidiFilter.java
@@ -0,0 +1,32 @@
+package oovu.events;
+
+import oovu.events.types.MidiEvent;
+
+public class MidiFilter extends Filter {
+    public final Integer channel;
+    public final Integer controller;
+
+    public MidiFilter(Integer channel, Integer controller) {
+        this.channel = channel;
+        this.controller = controller;
+    }
+
+    @Override
+    public boolean is_valid_event(Event event) {
+        if (!(event instanceof MidiEvent)) {
+            return false;
+        }
+        MidiEvent midi_event = (MidiEvent) event;
+        if (this.channel != null) {
+            if (midi_event.channel != this.channel) {
+                return false;
+            }
+        }
+        if (this.controller != null) {
+            if (midi_event.controller != this.controller) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
