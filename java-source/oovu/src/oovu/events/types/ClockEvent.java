4be3851b5ccda9233e34e464803d4b715a62c335
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-12-09 17:02:12 -0500

    Added oovu.events.types.ClockEvent.

---
+++ java-source/oovu/src/oovu/events/types/ClockEvent.java
@@ -0,0 +1,12 @@
+package oovu.events.types;
+
+import oovu.events.Event;
+
+public class ClockEvent extends Event {
+  
+    public final double current_time;
+    
+    public ClockEvent() {
+        this.current_time = System.currentTimeMillis(); 
+    }
+}
